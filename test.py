50c2561347cefbb71e7f163813eab2747f7ecc4c
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2025-01-10 23:28:13 -0800

    add Parameters, to be used with parametrize

--- test.py
+++ test.py
@@ -10,3 +10,14 @@ except:
 def parametrize(argnames, argvalues, *a, **kw):
     kw.setdefault("ids", list(map(str, argvalues)))
     return pytest.mark.parametrize(argnames, argvalues, *a, **kw)  # type: ignore
+
+
+class Parameters(tuple):
+    def __new__(cls, description, *argvalues):
+        assert isinstance(description, str)
+        parameters = tuple.__new__(Parameters, argvalues)
+        parameters._str = description  # type: ignore
+        return parameters
+
+    def __str__(self):
+        return self._str  # type: ignore
