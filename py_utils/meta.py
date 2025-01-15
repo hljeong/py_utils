b86d1f90153857289344444f4a8f2a3fd1ced794
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2025-01-15 01:37:09 -0800

    no more roundable way to get most derived metaclass

--- py_utils/meta.py
+++ py_utils/meta.py
@@ -1,14 +1,12 @@
-from types import new_class, prepare_class
+from types import new_class
 
 
 def meta(**kwds):
     def decorator(cls):
-        # roundabout way to get the most derived metaclass of superclasses of the decorated class...
-        meta = prepare_class("", cls.__bases__)[0]
         return new_class(
             cls.__name__,
             cls.__bases__,
-            dict(metaclass=type("Meta", (meta,), kwds)),
+            dict(metaclass=type("Meta", (cls.__class__,), kwds)),
             lambda ns: ns.update(cls.__dict__),
         )
 
