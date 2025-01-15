7f52a8de6f16ef465f077f43b6499215dfb73e52
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2025-01-14 23:47:23 -0800

    add metaclass factory for repr

--- py_utils/meta.py
+++ py_utils/meta.py
@@ -1,7 +1,19 @@
-def meta(repr=None):
-    d = dict()
+from types import new_class
+
+from .parametrize import parametrize
+
+
+# todo: is >1 bases ever unavoidable? i.e., needs to be supported?
+@parametrize("base")
+def Meta(*, repr=None, base=None):
+    name = "Meta" if not base else f"Meta[{base.__name__}]"
+
+    base = base or type
+
+    kwds = dict()
 
     if repr is not None:
-        d["__repr__"] = lambda _: repr
+        kwds["__repr__"] = lambda _: repr
 
-    return type("meta", (type,), d)
+    # this is adding "Meta" to the wrong end... but it works
+    return new_class(name, (base,), dict(metaclass=type(f"Meta{name}", (base,), kwds)))
